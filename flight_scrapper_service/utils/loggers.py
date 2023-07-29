4483d056c9781c7d7ddafbd5f976c36be16ef46f
JonathanMant <jonathan_mantilla@hotmail.com>
2023-07-29 17:47:35 -0500

    Add loggers

---
+++ flight_scrapper_service/utils/loggers.py
@@ -0,0 +1,13 @@
+import logging
+
+def setup_logger():
+    logger = logging.getLogger('my_logger')
+    logger.setLevel(logging.INFO)
+
+    log_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    file_handler = logging.FileHandler('app.log')
+    file_handler.setFormatter(log_formatter)
+    logger.addHandler(file_handler)
+
+    return logger
