6d8b5fa8056f3971f134ea6ff72c20128af1282c
JonathanMant <jonathan_mantilla@hotmail.com>
2023-09-02 22:03:19 -0500

    Add TODO about change method

--- flight_scrapper_service/main.py
+++ flight_scrapper_service/main.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify
 import random
 
 from flights.domain.flight_finder import FlightFinder
-from flights.infrastructure.flight_finder import FlightFinderWithConstant
+from flights.infrastructure.flight_finder import (
+    FlightFinderWithConstant
+    )
 
 app = Flask(__name__)
 
@@ -14,8 +16,8 @@ def hello_world():
 
 @app.route("/flights", methods=['GET'])
 def flights():
+    # TODO: Change method when filter are implemented
     id_fly = random.randint(1, 7)
-
     data, status = FlightFinder(
         repository=FlightFinderWithConstant()
     ).search(
