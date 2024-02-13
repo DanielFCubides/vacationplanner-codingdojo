610d8209a243334d3174dba660f58ca33ab3af87
Sebastian Rocha <jsebas791@gmail.com>
2024-02-12 19:29:53 -0500

    feat: search flights

--- flight_scrapper_service/main.py
+++ flight_scrapper_service/main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask import Flask, jsonify
 import random
 
@@ -20,6 +22,24 @@ def airline1():
     repository = airline_scrapper.AirlineSearch()
     result = repository.get_flights()
     return result
+
+
+@app.route("/flights_search")
+def flight_finder():
+    initial_departure_time = datetime(2024, 2, 13)
+    arrival_departure_time = datetime(2024, 2, 23)
+    from_ = 'BOG'
+    to = 'CTG'
+    passengers = 1
+    repository = airline_scrapper.AirlineSearch()
+    result = repository.get_flights_search(
+        initial_departure_time,
+        arrival_departure_time,
+        from_,
+        to,
+        passengers
+    )
+    return jsonify({'title': result})
 
 
 @app.route("/flights", methods=['GET'])
