4fd56da1ec39fc2d21183169b286af66e26302ae
DanielFCubides <dfcubidesc@unal.edu.co>
2026-03-16 20:49:52 -0500

    Remove breakpoint

--- auth_service/presentation/auth_router.py
+++ auth_service/presentation/auth_router.py
@@ -33,7 +33,6 @@ async def login(
         )
         return redirect
     except Exception as e:
-        breakpoint()
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"Login failed: {str(e)}"
