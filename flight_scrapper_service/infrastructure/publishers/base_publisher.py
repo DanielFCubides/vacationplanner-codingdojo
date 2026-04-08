3bb7668b22ac24716c793806afb9155ecc6697df
Sebastian Rocha <jsebas791@gmail.com>
2026-04-07 19:17:25 -0500

    fix: flight_service project structure

--- flight_scrapper_service/infrastructure/publishers/base_publisher.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from domain.models import SearchParams
-
-
-class SearchPublisher(ABC):
-    @abstractmethod
-    def publish_search_params(self, search_params: SearchParams) -> None:
-        """Takes the search params and send it to another system for handle it"""
-        ...
--- flight_scrapper_service/utils/loggers.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-def setup_logger(*, logger_name: str):
-    logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.INFO)
-    logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-
-    return logger
